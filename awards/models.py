1cf9cf7c11b896ba9d9aabf3cba568bb1cf675bd
Micah <micahkimathi@gmail.com>
2019-07-01 11:49:44 +0300

    Created a model for the products in the models.py file and made migrations

--- awards/models.py
+++ awards/models.py
@@ -31,3 +31,8 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+class MoringaMerch(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
